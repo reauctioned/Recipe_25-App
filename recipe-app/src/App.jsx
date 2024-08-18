f75020e37bbbaeba8a8e636b9d28a538dbd41b8f
Saurav <sauravgamer441@gmail.com>
2024-08-18 21:47:10 +0545

    add routes to pages

--- recipe-app/src/App.jsx
+++ recipe-app/src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
-import {Routes, Route} from 'react-router-dom'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import './App.css'
-import NavBar from './components/navbar'
+import "./App.css";
+import NavBar from "./components/navbar";
+import HomePage from "./pages/home/home";
+import Detail from "./pages/details/detail";
+import Favorite from "./pages/favorite/fav";
 
 function App() {
-
   return (
     <>
-     <div className='min-h-screen p-6 bg-white text-gray-600 text-lg'>
-        <NavBar/>
-     </div>
+      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/favorite" element={<Favorite />} />
+          <Route path="/recipe-item/:id" element={<Detail />} />
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
