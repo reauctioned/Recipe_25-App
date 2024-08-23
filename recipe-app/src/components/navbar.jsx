be3c5be90d0bcce92e0612730cf251c94f523030
Saurav <sauravgamer441@gmail.com>
2024-08-23 22:27:00 +0545

    style input and searchIcon

--- recipe-app/src/components/navbar.jsx
+++ recipe-app/src/components/navbar.jsx
@@ -8,18 +8,18 @@ export default function NavBar() {
     <nav className="flex  top-0  w-[100%]  justify-between items-center py-4 bg-white drop-shadow-md  flex-col lg:flex-row gap-5 lg:gap-0">
       <div className="flex items-center gap-4"> 
     
-        <img src="/public/logo.png" alt="logo" className="w-12 h-12 p-1 ml-6" />
-        <h1 className="font-bold text-3xl  bg-gradient-to-r from-[#fbb202] via-[#fc8402] to-[#fe460d] bg-clip-text text-transparent">
+        <img src="/public/logo.png" alt="logo" className="w-12 h-12 p-1 ml-2" />
+        <h1 className="font-bold text-3xl mr-7 bg-gradient-to-r from-[#fbb202] via-[#fc8402] to-[#fe460d] bg-clip-text text-transparent">
           RecipeLair
         </h1>
       </div>
       <form >
-        <SearchIcon/>
+        <SearchIcon sx={{ color: 'orange' }} />
         <input
           type="text"
           name="search"
           placeholder="Enter Items"
-          className="border rounded p-2  ml-8 lg:mr-5"
+          className="ml-2 border border-orange-500 border-t-0 border-l-0 border-r-0 border-b-2 focus:outline-none p-2  ml-8 lg:mr-5"
         />
       </form>
     </nav>
